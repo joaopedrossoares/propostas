908e26258be60b528922b82da02d5213d852c47c
Joao Pedro <jaopss@outlook.com>
2020-08-22 00:16:59 -0300

    adicionando rota

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/upload','OfferController@uploadFile')->name('offers.upload');
